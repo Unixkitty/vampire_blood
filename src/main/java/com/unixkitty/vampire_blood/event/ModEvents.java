fbf0acd045054065f4e54f621a4088a0f1d9e9c2
Unixkitty <uni@unixkitty.com>
2023-04-30 19:54:15 +0400

    Small changes

--- src/main/java/com/unixkitty/vampire_blood/event/ModEvents.java
+++ src/main/java/com/unixkitty/vampire_blood/event/ModEvents.java
@@ -2,12 +2,11 @@ package com.unixkitty.vampire_blood.event;
 
 import com.unixkitty.vampire_blood.Config;
 import com.unixkitty.vampire_blood.VampireBlood;
+import com.unixkitty.vampire_blood.VampireUtil;
 import com.unixkitty.vampire_blood.capability.VampirePlayerData;
 import com.unixkitty.vampire_blood.capability.VampirePlayerProvider;
-import net.minecraft.resources.ResourceLocation;
 import net.minecraft.server.level.ServerPlayer;
 import net.minecraft.world.damagesource.IndirectEntityDamageSource;
-import net.minecraft.world.entity.Entity;
 import net.minecraft.world.entity.LivingEntity;
 import net.minecraft.world.entity.MobType;
 import net.minecraft.world.entity.ai.goal.Goal;
@@ -18,14 +17,9 @@ import net.minecraft.world.entity.player.Player;
 import net.minecraft.world.item.TieredItem;
 import net.minecraft.world.item.Tiers;
 import net.minecraftforge.common.capabilities.RegisterCapabilitiesEvent;
-import net.minecraftforge.event.AttachCapabilitiesEvent;
-import net.minecraftforge.event.TickEvent;
 import net.minecraftforge.event.entity.EntityJoinLevelEvent;
 import net.minecraftforge.event.entity.living.LivingHurtEvent;
-import net.minecraftforge.event.entity.player.PlayerEvent;
-import net.minecraftforge.eventbus.api.EventPriority;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
-import net.minecraftforge.fml.LogicalSide;
 import net.minecraftforge.fml.common.Mod;
 
 import java.util.function.BiFunction;
@@ -41,30 +35,10 @@ public class ModEvents
         {
             event.setAmount(event.getAmount() * 1.25F);
         }
-    }
 
-    @SubscribeEvent
-    public static void onAttachCapabilityPlayer(final AttachCapabilitiesEvent<Entity> event)
-    {
-        if (event.getObject() instanceof Player)
+        if (!event.getEntity().getLevel().isClientSide() && event.getEntity() instanceof Player player && VampireUtil.isVampire(player) && event.getSource().isFire())
         {
-            if (!event.getObject().getCapability(VampirePlayerProvider.VAMPIRE_PLAYER).isPresent())
-            {
-                event.addCapability(new ResourceLocation(VampireBlood.MODID, "vampirism"), new VampirePlayerProvider());
-            }
-        }
-    }
-
-    @SubscribeEvent(priority = EventPriority.HIGH)
-    public static void onPlayerCloned(final PlayerEvent.Clone event)
-    {
-        if (!event.getEntity().getLevel().isClientSide())
-        {
-            event.getOriginal().reviveCaps();
-
-            VampirePlayerData.copyData(event.getOriginal(), event.getEntity(), event.isWasDeath());
-
-            event.getOriginal().invalidateCaps();
+            event.setAmount(event.getAmount() > 0 ? event.getAmount() * 2 : event.getAmount());
         }
     }
 
@@ -114,18 +88,4 @@ public class ModEvents
         event.register(VampirePlayerData.class);
     }
 
-    @SubscribeEvent
-    public static void onPlayerTick(final TickEvent.PlayerTickEvent event)
-    {
-        if (event.side == LogicalSide.SERVER && event.phase == TickEvent.Phase.END)
-        {
-            event.player.getCapability(VampirePlayerProvider.VAMPIRE_PLAYER).ifPresent(vampirePlayerData ->
-            {
-                if (!event.player.isCreative() && !event.player.isSpectator())
-                {
-                    vampirePlayerData.tick(event.player);
-                }
-            });
-        }
-    }
 }
--- src/main/java/com/unixkitty/vampire_blood/event/VampirePlayerEvents.java
+++ src/main/java/com/unixkitty/vampire_blood/event/VampirePlayerEvents.java
@@ -2,15 +2,22 @@ package com.unixkitty.vampire_blood.event;
 
 import com.unixkitty.vampire_blood.VampireBlood;
 import com.unixkitty.vampire_blood.VampireUtil;
+import com.unixkitty.vampire_blood.capability.VampirePlayerData;
+import com.unixkitty.vampire_blood.capability.VampirePlayerProvider;
+import net.minecraft.resources.ResourceLocation;
 import net.minecraft.server.level.ServerPlayer;
 import net.minecraft.world.effect.MobEffectCategory;
 import net.minecraft.world.effect.MobEffects;
+import net.minecraft.world.entity.Entity;
 import net.minecraft.world.entity.player.Player;
-import net.minecraftforge.event.entity.living.LivingHurtEvent;
+import net.minecraftforge.event.AttachCapabilitiesEvent;
+import net.minecraftforge.event.TickEvent;
 import net.minecraftforge.event.entity.living.MobEffectEvent;
+import net.minecraftforge.event.entity.player.PlayerEvent;
 import net.minecraftforge.eventbus.api.Event;
 import net.minecraftforge.eventbus.api.EventPriority;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
+import net.minecraftforge.fml.LogicalSide;
 import net.minecraftforge.fml.common.Mod;
 
 @Mod.EventBusSubscriber(modid = VampireBlood.MODID)
@@ -38,11 +45,42 @@ public class VampirePlayerEvents
     }
 
     @SubscribeEvent
-    public static void onLivingHurtEvent(final LivingHurtEvent event)
+    public static void onAttachCapabilityPlayer(final AttachCapabilitiesEvent<Entity> event)
     {
-        if (!event.getEntity().getLevel().isClientSide() && event.getEntity() instanceof Player player && VampireUtil.isVampire(player) && event.getSource().isFire())
+        if (event.getObject() instanceof Player)
         {
-            event.setAmount(event.getAmount() > 0 ? event.getAmount() * 2 : event.getAmount());
+            if (!event.getObject().getCapability(VampirePlayerProvider.VAMPIRE_PLAYER).isPresent())
+            {
+                event.addCapability(new ResourceLocation(VampireBlood.MODID, "vampirism"), new VampirePlayerProvider());
+            }
+        }
+    }
+
+    @SubscribeEvent(priority = EventPriority.HIGH)
+    public static void onPlayerCloned(final PlayerEvent.Clone event)
+    {
+        if (!event.getEntity().getLevel().isClientSide())
+        {
+            event.getOriginal().reviveCaps();
+
+            VampirePlayerData.copyData(event.getOriginal(), event.getEntity(), event.isWasDeath());
+
+            event.getOriginal().invalidateCaps();
+        }
+    }
+
+    @SubscribeEvent
+    public static void onPlayerTick(final TickEvent.PlayerTickEvent event)
+    {
+        if (event.side == LogicalSide.SERVER && event.phase == TickEvent.Phase.END)
+        {
+            event.player.getCapability(VampirePlayerProvider.VAMPIRE_PLAYER).ifPresent(vampirePlayerData ->
+            {
+                if (!event.player.isCreative() && !event.player.isSpectator())
+                {
+                    vampirePlayerData.tick(event.player);
+                }
+            });
         }
     }
 }
